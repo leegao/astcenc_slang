18943a55908058997a8196ee877317d4894f63fa
leegao <lg342@cornell.edu>
2025-12-10 21:34:56 -0600

    Add test harness for the hard test

--- hard.sh
+++ hard.sh
@@ -1,1 +1,2 @@
-uv run python astc_encoder.py -o reconstructed_test_2p.png --use_2p --m 1000 test_rgb_hard_1.png
+uv run python astc_encoder.py -o reconstructed_test_1p.png --m 100 test_rgb_hard_1.png
+uv run python astc_encoder.py -o reconstructed_test_2p.png --use_2p --m 100 test_rgb_hard_1.png
